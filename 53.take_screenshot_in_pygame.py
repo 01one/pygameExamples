5f8b4791b73f1503e8d6eeef139f0bc47d4baccd
01one <84787013+01one@users.noreply.github.com>
2024-03-12 15:11:07 +0600

    Create 53.take_screenshot_in_pygame.py

---
+++ 53.take_screenshot_in_pygame.py
@@ -0,0 +1,56 @@
+import pygame
+import sys
+from pygame.locals import *
+
+pygame.init()
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((400, 400), RESIZABLE)
+
+
+White = (255,255,255)
+SeaGreen = (46, 139, 87)
+Green = (0, 128, 0)
+
+def draw_button(surface, text='button', font=None, font_color=White, bg_color=SeaGreen, position=(0, 0), size=(150, 40)):
+	button_surface = pygame.Surface(size)
+	button_surface.fill(bg_color)
+	
+	if font is None:
+		font = pygame.font.Font(None, 22)
+	text_render = font.render(text, True, font_color)
+	text_rect = text_render.get_rect(center=(size[0] / 2, size[1] / 2))
+	button_surface.blit(text_render, text_rect)
+	
+	rect = surface.blit(button_surface, position)
+	
+	return rect
+
+game_running = True
+while game_running:
+	clock.tick(60)
+	screen.fill(White)
+	
+
+	for i in range(1, 5):
+		for j in range(1, 5):
+			pygame.draw.rect(screen, SeaGreen, (i * 60, j * 60, 50, 50), border_radius=10)
+			pygame.draw.rect(screen, Green, (i * 60, j * 60, 50, 50), 5, border_radius=10)
+	
+
+	button_rect = draw_button(screen, "Take Screenshot", position=(100, 320))
+
+
+	pygame.display.update()
+	
+
+	for event in pygame.event.get():
+		if event.type == QUIT:
+			pygame.quit()
+			sys.exit()
+		elif event.type == MOUSEBUTTONDOWN:
+			mouse_pos = pygame.mouse.get_pos()
+			if button_rect.collidepoint(mouse_pos):
+				#When the button clicked it will save the screenshot
+				pygame.image.save(screen, "screenshot.png")
+
+				print("Screenshot saved as screenshot.png")
